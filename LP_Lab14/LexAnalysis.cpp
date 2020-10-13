420323f7ecee8c7700262db41a3c1f36d425eb40
Harwex213 <harwex213@mail.ru>
2020-10-13 14:23:33 +0300

    Lab14-1.5.5(Make LexTable - done)

--- LP_Lab14/LexAnalysis.cpp
+++ LP_Lab14/LexAnalysis.cpp
@@ -26,12 +26,22 @@ namespace LexAnalysis
 			if (!FST::execute(automats[string[0]]))
 			{
 				entry.lexema[LEXEMA_FIXSIZE - 1] = LEX_ID;
-				if (!FST::execute(automats['i']))
+				if (!FST::execute(automats.KeyWord[IDENTIFY_INDEX]))
 					throw ERROR_THROW_IN(111, entry.sn, entry.psn);
 			}
 			Add(lextable, entry);
 		}
 
+		int counter = 0;
+		for (int i = 0; i < in.lines; i++)
+		{
+			cout << lextable.table[counter].sn << ' ';
+			while (lextable.table[counter].sn == i + 1)
+			{
+				cout << lextable.table[counter++].lexema;
+			}
+			cout << endl;
+		}
 	};
 
 	void FillIdTable(const In::IN& in, const LT::LexTable& lextable, IT::IdTable& idtable)
